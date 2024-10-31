5aa1928a5d11a13674544fe002323ba96e42feb3
Denis Nunes <denisfln@gmail.com>
2024-10-31 00:17:15 -0300

    ajustes na exibição

--- gradeoverview/block_gradeoverview.php
+++ gradeoverview/block_gradeoverview.php
@@ -15,15 +15,15 @@ class block_gradeoverview extends block_base
             return $this->content;
         }
 
-        global $COURSE, $DB, $OUTPUT;
+        global $COURSE;
 
         $this->content = new stdClass;
         $this->content->text = '';
 
         // Gera o link para o download do CSV
         $url = new moodle_url('/blocks/gradeoverview/download_grades.php', array('courseid' => $COURSE->id));
-        $this->content->text .= html_writer::link($url, 'Baixar Planilha de Notas', array('class' => 'btn btn-primary'));
-        $this->content->text .= html_writer::link('https://dead.uern.br', 'Ver tutorial de utilização', array('class' => 'btn btn-secondary'));
+        $this->content->text .= html_writer::link($url, get_string('download_xls', 'block_gradeoverview'), array('class' => 'btn btn-primary'));
+        $this->content->text .= html_writer::link('https://dead.uern.br', get_string('view_tutorial', 'block_gradeoverview'), array('class' => 'btn btn-secondary'));
 
         return $this->content;
     }
--- gradeoverview/lang/en/block_gradeoverview.php
+++ gradeoverview/lang/en/block_gradeoverview.php
@@ -1,2 +1,4 @@
 <?php
 $string['pluginname'] = 'Notas para SIGAA';
+$string['download_xls'] = 'Baixar Planilha de Notas';
+$string['view_tutorial'] = 'Ver Tutorial de Utilização';
